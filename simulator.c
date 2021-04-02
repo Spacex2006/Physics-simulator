d1848cd1643b054e750bf2f1e8cc670b10c07923
Spacex2006 <72301663+Spacex2006@users.noreply.github.com>
2021-04-02 15:47:24 +0530

    Update simulator.c

--- simulator.c
+++ simulator.c
@@ -8,16 +8,16 @@ int main()
 {
 
 	double msec;
-	double b,a=-1;
+	int b;
 	clock_t before = clock();
 
 	//starting simulation part
-	double q1, q2, r, m1, m2, someconstant1, someconstant2, s1, s2, s3, s4, a1, a2, position1, position2, u1=0, u2=0, t=0.1, p1, p2, v;
+	double q1, q2, r, m1, m2, someconstant1, someconstant2, s1, s2, s3, s4, a1, a2, position1=0, position2=1, u1=0, u2=0, t=0.00001, p1, p2, v;
 	printf("enter value of charge  of first particle , charge of second particle , distance between them , mass of first particle and mass of second particle (warning:-please put values in order or it may give wrong results also keep in mind that input is less than 6 digit and if your value is more or less than that you may use other unit and output will also be in that also keep in mind that the distance which will come out would be relative to the point in between them)\n");
 	scanf("%lf %lf %lf %lf %lf",&q1,&q2,&r,&m1,&m2);
 	someconstant1=(k*q1*q2+G*m1*m2)/(m1);
 	someconstant2=(k*q1*q2+G*m1*m2)/(m2);
-	for(msec=0;position1>position2;)
+	for(msec=0;position1<position2;)
 	{
     	clock_t difference = clock() - before;
     	msec = difference * 1000 / CLOCKS_PER_SEC;
@@ -30,18 +30,16 @@ int main()
 		s2=u2*t+1/2*a2*t*t;
 		position1=s1-r/2;
 		position2=r/2-s2;
-   		if(a!=b)
    		{
    			system("cls");
 			printf("\nobject1:- \npostion:- %lf\nvelocity:- %lf\ntime:- %lf\n",position1,u1,t);
 			printf("\nobject2:- \npostion:- %lf\nvelocity:- %lf\ntime:- %lf\n",position2,u2,t);
-			printf("\ntime done: %lf\n",b);
+			printf("\ntime done: %d\n",b);
 		}
    		s3=s3+s1;
 		s4=s4+s2;
 		r=r-s1-s2;
-		a=b;
-		t=t+0.0001;		
+		t=t+0.00001;		
 	}
 	printf("\nthe particles have crossed each others path or went through each other or just had a collision at t= %lf last position value we got was %lf of object 1 and %lf of object 2 with %lf time being passed since programn excuted",t,position1,position2,b);
 	return 0;
