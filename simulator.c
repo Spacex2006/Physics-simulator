83ba37344b7c58a190e050a18803f14d8a3daf8b
Spacex2006 <72301663+Spacex2006@users.noreply.github.com>
2021-06-16 15:41:15 +0530

    Update simulator.c

--- simulator.c
+++ simulator.c
@@ -17,26 +17,25 @@
 #define reset   "\x1b[0m"
 int main() 
 {
-	lol:
+	home_page:
 	double msec;
 	int a,b,c,d,e,h;
 	char f,g;
 	int iterations;
-	clock_t before = clock();
 	//starting simulation part
-	double q1, q2, r, m1, m2, someconstant1, someconstant2, s1, s2, s3, s4, a1, a2, position1=0, position2=1, position3, position4, u1=0, u2=0, t=0, t1, p1, p2, v;
+	double q1, q2, r, m1, m2, someconstant1, someconstant2, someconstant, s1, s2, s3, s4, a1, a2, position1=0, position2=1, position3, position4, u1=0, u2=0, t=0, t1, p1, p2, v, t2;
 	system("cls");
 	printf(yellow"WELCOME\npress some thing to continue");
 	getch();
 	system("cls");
-	printf("press 1 for default system \npress 2 to manualy select all settings\n");
+	printf("press 1 for default system \npress 2 to manualy select all settings\n\n\n\npls press enter after after every input\nthank u\n");
 	scanf("%d",&e);
 	if(e==2)
 	{
 		system("cls");
-		printf("Sorry but this part of simulator is still in development press any key to go to home");
-		getch();
-		goto lol;
+		printf("pls type number for the setting u want to change from the following options");
+		
+		goto home_page;
 	}
 	system("cls");
 	printf("which forces do you want\npress 1 for only gravity\npress 2 for only electromagnetism\npress 3 for both\n");
@@ -73,22 +72,17 @@ int main()
 		printf("\nplz enter charge of object2\n");
 		scanf("%lf",&q2);
 	}
+	someconstant1=(k*q1*q2+G*m1*m2)/(m1);
+	someconstant2=(k*q1*q2+G*m1*m2)/(m2);
 	printf("\nplz enter distance between objects\n");
-		scanf("%lf",&r);
-		if(r<=100)
-		t1=0.01;
-		else if(r<=1000)
-		t1=0.1;
-		else if(r<=10000)
-		t1=1;
-		else if(r<=100000)
-		t1=10;
-		else if(r<=1000000)
-		t1=100;
-		else if(r<=10000000)
-		t1=1000;
-		someconstant1=(k*q1*q2+G*m1*m2)/(m1);
-		someconstant2=(k*q1*q2+G*m1*m2)/(m2);
+	scanf("%lf",&r);
+	someconstant=(k*q1*q2+G*m1*m2);
+	t1=someconstant/10000000;
+	clock_t before = clock();
+	printf("t1-%d\nt2-%d",t1,t2);
+	getch();
+	u1=0;
+	u2=0;
 	for(msec=0;position1<position2;)
 	{
 		position4=position2;
@@ -109,7 +103,7 @@ int main()
 		printf("\nobject1:- \npostion:- %lf\nvelocity:- %lf\ntime:- %lf\n",position3,u1,t);
 		printf("\nobject2:- \npostion:- %lf\nvelocity:- %lf\ntime:- %lf\n",position4,u2,t);
 		printf("\ntime done: %d\n",b);
-		printf("press p to pause \ne to end  \npress h to go to home(remember this simulation2 will be executed)");
+		printf("press p to pause \ne to end  \npress h to go to home(remember this simulation will be executed)");
 		if(kbhit()) {
 			char ch=getch();
 			if(ch=='p') {
@@ -118,7 +112,7 @@ int main()
 			}
 			if(ch=='h')
 			{
-				goto lol;
+				goto home_page;
 			}
 			if(ch=='e') 
 			{
@@ -152,7 +146,7 @@ int main()
 		r=r-s1-s2;	
 	}
 	printf("\nthe particles have crossed each others path or went through each other or just had a collision at t= %lf last position value we got was %lf of object 1 and %lf of object 2 with %lf time being passed since programn excuted",t,position3,position4,b);
-	printf("\npress h to go to home page\npress e to exit\nPLZ RESPOND WITHIN  1MIN OR ELSE CODE WILL AUTOMATICLY BE EXECUTED\n");
+	printf("\npress h to go to home page\npress e to exit\nRESPOND WITHIN  1MIN OR ELSE CODE WILL AUTOMATICLY BE EXECUTED\n");
 	msec=0;
 	for(a=0;a<60;iterations++)
 	{
@@ -161,10 +155,31 @@ int main()
 			char ch=getch();
 			if(ch=='h') 
 			{
-				goto lol;
+				goto home_page;
 			}
 			if(ch=='e')
 			{
+				clock_t before = clock();
+				b=0;
+				d=0;
+				system("cls");
+				printf("thank you for using our simulator \nit would be appericitated if u can give us your feedback at tarsh006@gmail.com\n");
+				for(a=0;a<10;iterations++)
+				{
+					clock_t difference = clock() - before;
+    				msec = difference * 1000 / CLOCKS_PER_SEC;
+    				a=msec/1000;
+    				a=a-b;
+    				if(a!=d)
+    				{
+    					h=10-a;
+    					{
+    						printf("program will be executed in %d seconds \r",h);
+						}	
+					}
+    				d=a;
+    			}
+    			printf("                                      ");
 				return 0;
 			}
 		}
